51d63d84189727125ea606a334442829f01d9637
Wojciech Kaczmarek <wojtekk@kofeina.net>
2023-09-26 23:40:50 +0200

    initial commit - mutated from xflate

---
+++ main.go
@@ -0,0 +1,107 @@
+// xflate compresses or decompresses deflate stream.
+package main
+
+import (
+	"compress/zlib"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/spf13/pflag"
+)
+
+var prog, usageHead string
+
+func init() {
+	prog = "zlc"
+	usageHead = fmt.Sprintf(
+		"Usage: %s\t(reads stdin, outputs to stdout)", prog)
+}
+
+type config struct {
+	compress      bool
+	compressLevel int
+}
+
+func parseFlags(args []string) (conf config) {
+	var (
+		decompress bool
+		help       bool
+	)
+
+	flag := pflag.NewFlagSet("flags", pflag.ContinueOnError)
+	flag.SortFlags = false
+
+	flag.BoolVarP(&conf.compress, "compress", "z", true, "compress")
+	flag.BoolVarP(&decompress, "decompress", "d", false, "decompress")
+	flag.IntVarP(&conf.compressLevel, "level", "n", 6,
+		"compress level, -2..9")
+	flag.BoolVarP(&help, "help", "h", false,
+		"show this help and exit")
+	flag.Usage = func() {
+		fmt.Fprintln(flag.Output(), usageHead)
+		flag.PrintDefaults()
+	}
+
+	err := flag.Parse(args)
+	if err != nil {
+		die(2, err)
+	}
+	if len(flag.Args()) > 0 {
+		die(2, "unexpected args, use stdin/stdout")
+	}
+	if help {
+		flag.SetOutput(os.Stdout)
+		flag.Usage()
+		die(0)
+	}
+
+	if flag.Changed("compress") && flag.Changed("decompress") &&
+		conf.compress == decompress {
+		die(2, "conflicting flags -z and -d")
+	}
+	if flag.Changed("decompress") {
+		conf.compress = !decompress
+	}
+
+	return conf
+}
+
+func main() {
+	conf := parseFlags(os.Args[1:])
+
+	switch {
+	case conf.compress:
+		w, err := zlib.NewWriterLevel(os.Stdout, conf.compressLevel)
+		if err != nil {
+			die(1, "failed creating compress writer:", err)
+		}
+		_, err = io.Copy(w, os.Stdin)
+		if err != nil {
+			die(1, "compress:", err)
+		}
+		err = w.Close()
+		if err != nil {
+			die(1, "compress closing", err)
+		}
+
+	case !conf.compress:
+		r, err := zlib.NewReader(os.Stdin)
+		if err != nil {
+			die(1, "failed creating decompress reader:", err)
+		}
+		defer r.Close()
+		_, err = io.Copy(os.Stdout, r)
+		if err != nil {
+			die(1, "decompress:", err)
+		}
+	}
+}
+
+func die(exitcode int, msgs ...interface{}) {
+	if len(msgs) > 0 {
+		fmt.Fprint(os.Stderr, prog, ": ")
+		fmt.Fprintln(os.Stderr, msgs...)
+	}
+	os.Exit(exitcode)
+}
